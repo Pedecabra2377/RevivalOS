37c9d27828592abc7880c85adeae9b5b97a3bc21
Playerthree <150193341+Pedecabra2377@users.noreply.github.com>
2024-12-18 14:47:38 -0300

    Atualizar o login.js

--- login.js
+++ login.js
@@ -1,3 +1,11 @@
-window.addEventListener('load', () => {
-    //...
-})
+const form = document.getElementById('login-form');
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+if (username === 'admin@example.com' && password === '123456') {
+window.location.href = 'index.html';
+} else {
+alert('Credenciais inválidas!');
+}
+});
